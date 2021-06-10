25666e0930db3d9c40169d3c09999637c863f7d7
Dilip <dilipduraiswamy@gmail.com>
2021-06-10 12:07:04 +0530

    sun content service base

---
+++ sun-content-service/src/main/java/com/sunwonders/content/content/SunContentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sunwonders.content.content;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunContentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunContentServiceApplication.class, args);
+    }
+
+}
---
+++ sun-content-service/src/test/java/com/sunwonders/content/content/SunContentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunwonders.content.content;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunContentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
