fb419d0453bba47a7500ea6b25dee8d742436061
Dilip <dilipduraiswamy@gmail.com>
2021-06-10 09:53:31 +0530

    parent service base

---
+++ sun-parent-service/src/main/java/com/sun/wonders/parent/SunParentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sun.wonders.parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunParentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunParentServiceApplication.class, args);
+    }
+
+}
---
+++ sun-parent-service/src/test/java/com/sun/wonders/parent/SunParentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sun.wonders.parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunParentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
